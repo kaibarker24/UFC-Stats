81ed1f8759f1c14015db4faee005e5cb8fc61405
Kai Barker <kfb36@cornell.edu>
2025-02-22 20:01:34 -0500

    test

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/",  index_file="index.html", autorefresh=True)
 
 
+
 if __name__ == "__main__":
   app.run(threaded=True, port=9000)
